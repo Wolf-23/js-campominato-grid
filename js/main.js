8e15a31975e3188dc49d19ee7134d07714abe81b
Riccardo <lupinu.ric@gmail.com>
2022-07-13 17:24:51 +0200

    Project: Fine Progetto + Bonus

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let btnplay = document.getElementById('play');
+let grigliaElementi = document.getElementById('grid');
+let selectDom = document.getElementById('difficulty');
+
+btnplay.addEventListener('click', function() {
+    grigliaElementi.innerHTML = '';
+    generateGrid(selectDom.value);
+});
+        
+function generateGrid(level) {
+    let addedClass = '';
+    let cicles;
+    switch (level) {
+
+        case 'facile':
+            cicles = 100;
+            addedClass = 'wh-10';
+        break;
+        case 'media':
+            cicles = 81;
+            addedClass = 'wh-9';
+        break;
+        case 'difficile':
+            cicles = 49;
+            addedClass = 'wh-7';
+        break;
+    }
+
+    for (x = 1; x <= cicles; x++ ) {
+        let mySquare = generateSquare();
+        mySquare.append(x);
+        mySquare.classList.add(addedClass);
+        grigliaElementi.append(mySquare);
+        
+        changeBgBlue(mySquare);
+    }
+    
+}
+
+function generateSquare() {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    return square;
+}
+
+function changeBgBlue(mysquare) {
+    mysquare.addEventListener('click', function() {
+        this.classList.toggle('bg_blue');
+        console.log(mysquare.innerHTML)
+    });
+}
